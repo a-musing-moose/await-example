e4dbf99511df877b910a9c2847d521adffb58c95
Jonathan Moss <jmoss@snowballone.com.au>
2016-05-27 11:51:17 +1000

    Now get a json response

--- src/test.js
+++ src/test.js
@@ -1,8 +1,14 @@
 const fetch = require('isomorphic-fetch');
 
+if (process.argv.length != 3) {
+    console.error("You must specify a appid");
+    process.exit(-1);
+}
+const appid = process.argv[2];
+
 async function doGet() {
   try {
-    const response = await fetch('https://www.google.com');
+    const response = await fetch('http://api.openweathermap.org/data/2.5/weather?q=Sydney&appid=' + appid);
     const text = await response.text();
     console.log(text);
   } catch (e) {
